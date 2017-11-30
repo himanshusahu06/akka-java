950fe71604b65835d05970052e649c5d997d9d75
hsahu <hsahu@walmartlabs.com>
2017-11-30 14:44:12 +0530

    .

--- akka-basics/src/main/java/playing_with_actor/music/ActorCreation.java
+++ akka-basics/src/main/java/playing_with_actor/music/ActorCreation.java
@@ -30,9 +30,9 @@ class MusicPlayerActor extends AbstractActor {
 class MusicControllerActor extends AbstractActor {
 
 	private ActorRef musicPlayerActorRef;
-	
+
 	private Boolean playingSong = false;
-	
+
 	private Song currentPlaying = Song.NONE;
 
 	MusicControllerActor(ActorRef musicPlayerActorRef) {
@@ -47,9 +47,11 @@ class MusicControllerActor extends AbstractActor {
 
 	private void sendPlayCommand(MusicPlayerStartCommand startCommand) {
 		if (playingSong == true) {
-			System.out.println("[MusicControllerActor] : Sending switch music ["+ currentPlaying + "->" + startCommand.getSong()+ "] action message to MusicPlayer");
+			System.out.println("[MusicControllerActor] : Sending switch music [" + currentPlaying + "->"
+					+ startCommand.getSong() + "] action message to MusicPlayer");
 		} else {
-			System.out.println("[MusicControllerActor] : Sending start music action message to MusicPlayer - " + startCommand.getSong());
+			System.out.println("[MusicControllerActor] : Sending start music action message to MusicPlayer - "
+					+ startCommand.getSong());
 		}
 		MusicPlayAction musicPlayActionMessage = new MusicPlayAction(startCommand.getSong());
 		musicPlayerActorRef.tell(musicPlayActionMessage, self());
@@ -74,16 +76,17 @@ public class ActorCreation {
 
 		ActorRef musicPlayer = system.actorOf(Props.create(MusicPlayerActor.class), "MusicPlayer");
 
-		ActorRef musicController = system.actorOf(Props.create(MusicControllerActor.class, musicPlayer), "MusicController");
+		ActorRef musicController = system.actorOf(Props.create(MusicControllerActor.class, musicPlayer),
+				"MusicController");
 
 		musicController.tell(new MusicPlayerStartCommand(Song.LET_HER_GO), ActorRef.noSender());
-		
+
 		musicController.tell(new MusicPlayerStartCommand(Song.SHAPE_OF_YOU), ActorRef.noSender());
-		
+
 		musicController.tell(new MusicPlayerStopCommand(), ActorRef.noSender());
-		
+
 		musicController.tell(new MusicPlayerStartCommand(Song.RUDE), ActorRef.noSender());
-		
+
 		musicController.tell(new MusicPlayerStopCommand(), ActorRef.noSender());
 
 		system.terminate();
